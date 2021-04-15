1f8e512be492845a696f81633bc414c169864931
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-15 13:35:50 +0000

    [Add]books,editレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def edit
     @book=Book.new
     @book=Book.find(params[:id])
-    if @book.user=current_user
+    if @book.user==current_user
       render 'edit'
     else
       redirect_to book_path(@book.id)
