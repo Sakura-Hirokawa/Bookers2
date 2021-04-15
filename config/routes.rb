2b30ad9c9c2020f0173ccd33f42191b7246d2155
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-15 13:14:35 +0000

    [Add]booksのresources変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :books
+  resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :users, only:[:index, :show, :edit, :update, :destroy]
 end
