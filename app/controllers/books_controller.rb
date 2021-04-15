9ed15c8dd6b98919ed24f1d78d4838905e5b7ccd
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-15 08:48:57 +0000

    [Add]books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,21 +3,35 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+       redirect_to book_path(@book.id)
+    else
+       @user=current_user
+       @book=Book.all
+       render :index
+    end
   end
   
   def index
+    @user=current_user
     @book=Book.new
     @books=Book.all
   end
   
   def show
+    @book=Book.new
     @book=Book.find(params[:id])
+    @useer=@book.user
   end
   
   def edit
+    @book=Book.new
     @book=Book.find(params[:id])
+    if @book.user=current_user
+      render 'edit'
+    else
+      redirect_to book_path(@book.id)
+    end
   end
   
   def update
