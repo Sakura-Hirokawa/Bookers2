6cd8727d74e1881238cffb72905bd5016cf39ebb
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-16 00:15:58 +0000

    [Add]users,index,ユーザ一覧

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   def index
-    # @user誰がログインしているのか
+    # @user誰がログインしているのか、idつけるとエラー
     @user=current_user
+    @users=User.all
     @book=Book.new
     @books=Book.all
   end
   
   def show
+    @user=User.find(params[:id])
+    @book=Book.new
+    @book=@user.books
   end
   
   def edit
