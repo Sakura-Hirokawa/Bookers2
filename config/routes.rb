14c138e4c415e1e5e35b13f37991818e8c1accc0
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-17 05:57:53 +0000

    [Add]destroy削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only:[:index, :show, :edit, :update, :destroy]
+  resources :users, only:[:index, :show, :edit, :update]
 end
